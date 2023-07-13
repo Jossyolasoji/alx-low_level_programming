e2e378cf1dc2e6e9936e41032d42768d61432ef3
jossyolasoji <olasojijoseph6@gmail.com>
2023-07-13 15:18:06 -0700

    printf)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts
+ * Return: 0 (Good job)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
+
