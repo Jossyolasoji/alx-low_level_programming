f1078abcdfb87f22c700322bdc268d4aebf8afda
jossyolasoji <olasojijoseph6@gmail.com>
2023-07-14 18:58:31 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.>
+
+/**
+ * main - Entry mode
+ * Return:Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'Z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
+
